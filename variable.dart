dc1f469dd898aa226a3962d59e43d0d9b702cc25
kentigerc <kentigerc@gmail.com>
2024-02-24 10:33:25 +0300

    first commit

---
+++ variable.dart
@@ -0,0 +1,13 @@
+void main() {
+//declare variables
+  String name = 'dart class';
+  int age = 76;
+  double price = 100.98;
+  var year = 2024;
+  num students = 460;
+
+  print(name);
+  print(age);
+  print(year);
+  print(students);
+}
